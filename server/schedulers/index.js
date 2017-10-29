1764ffc104285ada3a444fc4f9b9cf49dd0ea279
Ridham Tarpara <ridham.tarpara@gmail.com>
2017-10-29 16:40:33 +0530

    cron updated

--- server/schedulers/index.js
+++ server/schedulers/index.js
@@ -4,8 +4,8 @@ const config = require('../../config');
 console.log(config.cronTime);
 const scheduler = schedule.scheduleJob(config.cronTime, () => {
 	try {
-		console.log('cron started\n\n\n\n\n\n');
-		// currencyController.getDataFromWeb();
+		// console.log('cron started\n\n\n\n\n\n');
+		currencyController.getDataFromWeb();
 	} catch (e) {
 		console.log(e);
 	}
